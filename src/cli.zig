45ca614ff30fed37a8dd3d7c50862c64232403fb
Jess Sullivan <Jess@sulliwood.org>
2026-04-30 19:57:13 -0400

    Bump version to 0.1.6 (#64)

--- src/cli.zig
+++ src/cli.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const types = @import("types.zig");
 
-pub const version = "0.1.5";
+pub const version = "0.1.6";
 
 pub const Command = union(enum) {
     exec: ExecArgs,
